9ae05337a79f92cc63771173a7821839340f9fd2
rh050 <hori050@gmail.com>
2024-10-14 18:07:35 +0300

    .

--- Assets/Scripts/Scriptable Object/HiderList.cs
+++ Assets/Scripts/Scriptable Object/HiderList.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "HiderList", menuName = "ScriptableObjects/HiderList", order = 1)]
 public class HiderList : ScriptableObject
 {
-    public GameObject objectsToHide; // The object to instantiate for hiding
-    public Vector3[] spawnPoints;    // List of spawn points
-    public int numberOfSpritesToCreate; // How many objects to spawn
-    public int numberOfSpawnPointsToCreate; // New public number for spawn points
+    public GameObject objectsToHide;
+    public Vector3[] spawnPoints;    
+    public int numberOfSpritesToCreate; 
+    public int numberOfSpawnPointsToCreate; 
 }
